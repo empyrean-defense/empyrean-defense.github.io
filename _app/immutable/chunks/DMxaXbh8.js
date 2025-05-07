61ec6bec26310e75e66c91a5974b658621172c67
var-sk <>
2025-05-06 23:54:19 -0400

    Updates

---
+++ _app/immutable/chunks/DMxaXbh8.js
@@ -0,0 +1,7 @@
+const e=`# Welcome to the Empyrean Defense Wiki\r
+\r
+Welcome to the official wiki for <a href="https://store.steampowered.com/app/3575690/Empyrean_Defense/" target="_blank">Empyrean Defense</a>\r
+\r
+## News\r
+\r
+Early Access Release is planned for July 24, 2025. For anyone interested in testing the game prior to Early Access please contact <a href="https://nighthawk-studios.com/" target="_blank">Nighthawk Studios</a>. `;export{e as default};
---
+++ _app/immutable/entry/start.7UdRD4nZ.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/oJHoR5Mf.js";export{o as load_css,r as start};
