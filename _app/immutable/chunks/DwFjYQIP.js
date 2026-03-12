9f01a82d5a934120832879c741988bfee686420d
var-sk <>
2026-03-11 22:33:40 -0400

    Updates

---
+++ _app/immutable/chunks/DwFjYQIP.js
@@ -0,0 +1,13 @@
+const e=`[Back](/abilities)\r
+\r
+# Teleport\r
+\r
+Displaces a group of enemies, instantly teleporting them back along their advance and disrupting their push. Does not affect bosses or airborne units\r
+\r
+## Stats\r
+- Type: Utility\r
+- Tier: II\r
+- Effect: Area\r
+- Rank Required: Sergeant (4)\r
+- Cooldown: 75s\r
+- Radius: 8m`;export{e as default};
