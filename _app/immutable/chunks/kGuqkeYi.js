ce9a19f9bca30837a48df620541c31267b39e34b
var-sk <>
2026-04-03 17:52:23 -0400

    Updates

---
+++ _app/immutable/chunks/kGuqkeYi.js
@@ -0,0 +1,32 @@
+const e=`# Achievements\r
+\r
+| Icon | Name | Description |\r
+|:----------:|:----------:|:----------:|\r
+| <img src="/achievements/Skillful_256_Enabled.png" title="Skillful" alt="Skillful" width="64px"> | Skillful | Spend a skill point |\r
+| <img src="/achievements/StandYourGround_256_Enabled.png" title="Stand your Ground" alt="Stand your Ground" width="64px"> | Stand your Ground | Succesfully defend a region |\r
+| <img src="/achievements/KnowledgeIsPower_256_Enabled.png" title="Knowledge is Power" alt="Knowledge is Power" width="64px"> | Knowledge is Power | Spend a skill point |\r
+| <img src="/achievements/CallToArms_256_Enabled.png" title="Sustained Rally" alt="Sustained Rally" width="64px"> | Sustained Rally | Maintain an active Call to Arms for 60 consecutive seconds |\r
+| <img src="/achievements/CashGrab_256_Enabled.png" title="Cash Grab" alt="Cash Grab" width="64px"> | Cash Grab | Collect 100,000 credits from coin drops |\r
+| <img src="/achievements/OneShotOneKill_256_Enabled.png" title="One Shot. One Kill" alt="One Shot. One Kill." width="64px"> | One Shot. One Kill. | Eliminate 1,000 enemies in a single shot using the [Sniper Tower](/sites/sniper_tower) |\r
+| <img src="/achievements/MinefieldMenace_256_Enabled.png" title="Minefield Menace" alt="Minefield Menace" width="64px"> | Minefield Menace | Eliminate 1,000 enemies using the [Landmines](/abilities/landmines) ability |\r
+| <img src="/achievements/LostAndFound_256_Enabled.png" title="Lost and Found" alt="Lost and Found" width="64px"> | Lost and Found | Recover a piece of Jax's past, left behind on the battlefield |\r
+| <img src="/achievements/TheInvisibleHand_256_Enabled.png" title="The Invisible Hand" alt="The Invisible Hand" width="64px"> | The Invisible Hand | Successfully defend a region without building any sites |\r
+| <img src="/achievements/Skyfall_256_Enabled.png" title="Skyfall" alt="Skyfall" width="64px"> | Skyfall | Eliminate 50 airborne enemies with the [Missile Strike](/abilities/missile_strike) ability |\r
+| <img src="/achievements/FusionEngineer_256_Enabled.png" title="Fusion Engineer" alt="Fusion Engineer" width="64px"> | Fusion Engineer | Build one of each Tier II military site in a single mission |\r
+| <img src="/achievements/ElenasNotebook_256_Enabled.png" title="Elena's Notebook" alt="Elena's Notebook" width="64px"> | Elena's Notebook | Find and collect Elena's lost field notebook |\r
+| <img src="/achievements/Explorer_256_Enabled.png" title="Explorer" alt="Explorer" width="64px"> | Explorer | Successfully defend a region in five different biomes |\r
+| <img src="/achievements/OneManArmy_256_Enabled.png" title="One-Man Army" alt="One-Man Army" width="64px"> | One-Man Army | Eliminate 300 enemies with a single site in a mission |\r
+| <img src="/achievements/NeverHadAChance_256_Enabled.png" title="Never had a Chance" alt="Never had a Chance" width="64px"> | Never had a Chance | Eliminate an enemy [Submarine](/enemies/submarine) before it has a chance to submerge beneath the waves |\r
+| <img src="/achievements/PluggedIn_256_Enabled.png" title="Plugged In" alt="Plugged In" width="64px"> | Plugged In | Build 10 power sites in a single mission |\r
+| <img src="/achievements/HiddenCache_256_Enabled.png" title="Hidden Cache" alt="Hidden Cache" width="64px"> | Hidden Cache | Find and collect a hidden supply cache |\r
+| <img src="/achievements/FieldMechanic_256_Enabled.png" title="Field Mechanic" alt="Field Mechanic" width="64px"> | Field Mechanic | Repair a total of 20,000 site hitpoints in a single mission |\r
+| <img src="/achievements/TheNegotiator_256_Enabled.png" title="The Negotiator" alt="The Negotiator" width="64px"> | The Negotiator | Complete 100 trades at the Market |\r
+| <img src="/achievements/GroundControl_256_Enabled.png" title="Ground Control" alt="Ground Control" width="64px"> | Ground Control | Eliminate 5,000 ground enemies |\r
+| <img src="/achievements/SteelAndSalt_256_Enabled.png" title="Steel and Salt" alt="Steel and Salt" width="64px"> | Steel and Salt | Eliminate 2,500 naval enemies |\r
+| <img src="/achievements/RestInGlory_256_Enabled.png" title="Rest in Glory" alt="Rest in Glory" width="64px"> | Rest in Glory | Pay your respects at the sarcophagus of Theron Solana, the late Supreme Ruler of the Empyrean Realm |\r
+| <img src="/achievements/MassProduction_256_Enabled.png" title="Mass Production" alt="Mass Production" width="64px"> | Mass Production | Manufacture 5,000 items at your industrial production sites |\r
+| <img src="/achievements/ToTheMoon_256_Enabled.png" title="To the Moon!" alt="To the Moon!" width="64px"> | To the Moon! | Successfully defend all regions |\r
+| <img src="/achievements/AllStar_256_Enabled.png" title="All Star" alt="All Star" width="64px"> | All Star | Earn all stars across every region and difficulty |\r
+| <img src="/achievements/SpeedDemon_256_Enabled.png" title="Speed Demon" alt="Speed Demon" width="64px"> | Speed Demon | Earn all Time Medals across every region and difficulty |\r
+| <img src="/achievements/Untouchable_256_Enabled.png" title="Untouchable" alt="Untouchable" width="64px"> | Untouchable | Earn all Perfection Medals across every region and difficulty |\r
+| <img src="/achievements/EmpyreanLegacy_256_Enabled.png" title="Empyrean Legacy" alt="Empyrean Legacy" width="64px"> | Empyrean Legacy | Earn all stars and medals across every region and difficulty |`;export{e as default};
