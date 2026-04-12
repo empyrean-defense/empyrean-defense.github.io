b16d674bf6a00f1710eaa2a1d692b4f1c700a9b2
var-sk <>
2026-04-12 02:03:05 -0400

    Updates

---
+++ _app/immutable/chunks/_8OZNXa6.js
@@ -0,0 +1,14 @@
+const r=`[Back](/abilities)\r
+\r
+# Scattershot\r
+\r
+Engulfs all enemies within an area in flames\r
+\r
+## Stats\r
+- Type: Attack\r
+- Tier: I\r
+- Effect: Area\r
+- Rank Required: Private First Class (2)\r
+- Cooldown: 20s\r
+- Damage: 600\r
+- Radius: 12m`;export{r as default};
